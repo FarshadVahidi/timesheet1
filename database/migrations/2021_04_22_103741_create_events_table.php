b8fe7454d1c0ffc9f194f7f615ee7c2ba37b76ba
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-04-22 12:48:39 +0200

    migrate events

---
+++ database/migrations/2021_04_22_103741_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->string('title', 100)->nullable();
+            $table->date('start');
+            $table->integer('allDay');
+            $table->float('hour');
+            $table->boolean('ferie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
